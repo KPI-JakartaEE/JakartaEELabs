f1d2e1a1344e32c708a4432f50e6878e67eb2f8b
AndriiHliuza <gl093770@gmail.com>
2025-04-28 14:47:52 +0300

    Create isPublic method to check for public endpoints in Filter

--- src/main/java/ua/kpi/jakartaee/rest/Filter.java
+++ src/main/java/ua/kpi/jakartaee/rest/Filter.java
@@ -9,6 +9,8 @@ import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 import ua.kpi.jakartaee.utils.Jwt;
 
+import java.util.stream.Stream;
+
 @Provider
 @Priority(Priorities.AUTHENTICATION)
 public class Filter implements ContainerRequestFilter {
@@ -17,7 +19,7 @@ public class Filter implements ContainerRequestFilter {
     public void filter(ContainerRequestContext requestContext) {
         String path = requestContext.getUriInfo().getPath();
 
-        if (path.startsWith("public")) {
+        if (isPublic(path)) {
             // Skip authentication for public endpoints
             return;
         }
@@ -27,5 +29,14 @@ public class Filter implements ContainerRequestFilter {
         if (authorizationHeader == null || !Jwt.validateToken(authorizationHeader)) {
             requestContext.abortWith(Response.status(Response.Status.UNAUTHORIZED).build());
         }
+    }
+
+    // Just in case we need more public endpoints, add all of them here
+    private boolean isPublic(String path) {
+        return Stream.of(
+                "public", // /api/public/*
+                "library",        // /api/library/*
+                "books"           // /api/books/*
+        ).anyMatch(path::startsWith);
     }
 }
